58d1636dcff11bbda029dea1f43f1eadc05d8f58
kishvd <kishorekumar.vd@cognizant.com>
2019-09-24 16:59:48 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  srcstring = "";
+  value = 0;
+  imgclick(val) {
+    console.log(val);
+    this.value = val;
+    this.srcstring = "https://cdn.jsdelivr.net/gh/kishvd/angular-Test@master/src/assets/image" + val + ".jpg";
+
+  }
 }
 
