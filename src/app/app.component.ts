879bd377bf0fa43d32bf48df7ed59231b719b44d
kishvd <kishorekumar.vd@cognizant.com>
2019-09-24 17:22:55 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ export class AppComponent {
   name = 'Angular';
   srcstring = "";
   value = 0;
+  enlarge = false;
   imgclick(val) {
-    console.log(val);
     this.value = val;
     this.srcstring = "https://cdn.jsdelivr.net/gh/kishvd/angular-Test@master/src/assets/image" + val + ".jpg";
 
+  }
+
+  enlargeclick(){
+    console.log("val");
+
+this.enlarge = !this.enlarge;
+
+  console.log(this.enlarge);
   }
 }
 
